2c64a021f80c6ab730bd4ae14b4fb9b45c0dfac9
wangyunyou <wangyunyou1995@163.com>
2020-04-03 14:04:47 +0800

    new css

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-Vue.prototype.$axios = axios
-Vue.config.productionTip = false 
+import "./assets/normalize.css";
 
-Vue.directive('title', {
-  inserted: function (el) {
-    document.title = el.dataset.title
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
+
+Vue.directive("title", {
+  inserted: function(el) {
+    document.title = el.dataset.title;
   }
-})
+});
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
